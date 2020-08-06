bcae1fcde08948c02061641b002249da7714ea67
JulieNobody <julie.nobody@gmail.com>
2020-08-06 17:58:43 +0200

    commit modifierProfil ok

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Class SortieController
@@ -26,37 +27,8 @@ class SortieController extends AbstractController
     }
 
     //fonction à déplacer dans le UserController
-    /**
-     * @Route("modifierProfil/{id}", name="user_modifierProfil", requirements={"id":"\d+"})
-     */
-    public function modifierProfil(User $user, Request $request, EntityManagerInterface $manager)
-    {
-        //création instance du formulaire
-        $userForm = $this->createForm(UserType::class, $user);
 
-        //association du formulaire avec la request
-        $userForm->handleRequest($request);
 
-        if ($userForm->isSubmitted() && $userForm->isValid())
-        {
-            $manager->persist($user);
-            $manager->flush();
 
-            $this->addFlash('success', 'Le profil a bien été mis à jour');
-
-            return $this->redirectToRoute('user_detailProfil');
-        }
-
-        return $this->render("user/modifierProfil.html.twig", ['userForm' => $userForm->createView()]);
-    }
-
-    /**
-     * @Route("detailProfil", name="user_detailProfil")
-     */
-    public function detailProfil()
-    {
-
-        return $this->render("user/detailProfil.html.twig");
-    }
 
 }
