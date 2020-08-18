a455aae86fd3d6ab3f7a86a3adce7cd47f07d6da
JulieNobody <julie.nobody@laposte.net>
2020-08-18 17:32:25 +0200

    admin - en cours

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class AdminController
+ * @package App\Controller
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("menu", name="admin_menu")
+     */
+    public function index()
+    {
+
+        return $this->render('admin/menu-admin.html.twig', []);
+    }
+}
